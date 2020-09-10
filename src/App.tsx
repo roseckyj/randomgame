b37d73043894aa3aee643230eecaa1495d534bcc
Jonáš Rosecký <rosecky.jonas@centrum.cz>
2020-09-10 21:46:39 +0300

    babylonjs

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,34 @@ import React from 'react';
 import io from 'socket.io-client';
 import { player } from './messageTypes';
 
+import '@babylonjs/core/Physics/physicsEngineComponent'; // side-effect adds scene.enablePhysics function
+import { Vector3, Mesh, Texture, FreeCamera, UniversalCamera } from '@babylonjs/core';
+import { Scene, Engine, SceneEventArgs } from 'react-babylonjs';
+
+const SPEED_CHANGE = 1;
+const SLOWING = 0.95;
+const KEY_UP = 38;
+const KEY_DOWN = 40;
+const KEY_LEFT = 37;
+const KEY_RIGHT = 39;
+
 interface IAppProps {}
 
-interface IAppState {}
+interface IAppState {
+    players: { [key: string]: player };
+    me: player | null;
+}
 
 class App extends React.Component<IAppProps, IAppState> {
-    gameCanvas = React.createRef<HTMLCanvasElement>();
-    context: CanvasRenderingContext2D | null = null;
     socket: SocketIOClient.Socket;
-
-    players: { [key: string]: player } = {};
-    me: player | null = null;
     index: string;
 
     keysPressed: number[] = [];
+
+    state: IAppState = {
+        players: {},
+        me: null,
+    };
 
     constructor(props: IAppProps) {
         super(props);
@@ -25,95 +39,182 @@ class App extends React.Component<IAppProps, IAppState> {
             this.update();
         }, 100);
 
-        setInterval(() => {
-            this.tick();
-        }, 10);
-
         document.addEventListener('keydown', (event) => {
-            event.preventDefault();
+            //event.preventDefault();
 
             if (!this.keysPressed.includes(event.keyCode)) {
                 this.keysPressed.push(event.keyCode);
             }
-
-            console.log(this.keysPressed);
         });
 
         document.addEventListener('keyup', (event) => {
-            event.preventDefault();
+            //event.preventDefault();
 
             if (this.keysPressed.includes(event.keyCode)) {
                 this.keysPressed = this.keysPressed.filter((keyCode) => keyCode !== event.keyCode);
             }
         });
+
+        window.addEventListener('resize', (event) => {
+            this.resize();
+        });
     }
 
     componentDidMount() {
-        if (this.gameCanvas.current) {
-            this.context = this.gameCanvas.current.getContext('2d');
-        }
         this.socket.on('id', (data: string) => {
             this.index = data;
             console.log('Received game ID: ', this.index);
         });
         this.socket.on('players', (data: { [key: string]: player }) => {
-            this.players = data;
-            if (this.me === null) {
-                this.me = this.players[this.index];
+            this.setState({ players: data });
+            if (!this.state.me) {
+                this.setState({ me: data[this.index] });
                 console.log('Created my character');
             }
-
-            if (this.context && this.gameCanvas.current) {
-                this.context.clearRect(0, 0, this.gameCanvas.current.width, this.gameCanvas.current.height);
-                Object.keys(this.players).forEach((key) => {
-                    const player = this.players[key];
-
-                    this.context!.fillStyle = '#FF0000';
-                    this.context!.fillRect(player.x, player.y, 20, 20);
-                });
-            }
         });
+        this.resize();
     }
 
     update() {
-        if (this.me) {
-            this.socket.emit('update', { id: this.index, content: this.me });
-        } else {
-            console.log("Can't update, player does not exist!", this.index);
+        if (this.state.me) {
+            this.socket.emit('update', { id: this.index, content: this.state.me });
         }
     }
 
-    tick() {
-        if (this.me) {
-            if (this.keysPressed.includes(37)) {
+    tick(deltaTimeRaw: number) {
+        const deltaTime = deltaTimeRaw * 0.1;
+
+        if (this.state.me) {
+            let me = this.state.me;
+
+            let movingX = false;
+            let movingY = false;
+
+            const diagonalModifier = Math.sqrt(2);
+
+            if (this.keysPressed.includes(KEY_LEFT) || this.keysPressed.includes(KEY_RIGHT)) {
+                movingX = true;
+            }
+            if (this.keysPressed.includes(KEY_UP) || this.keysPressed.includes(KEY_DOWN)) {
+                movingY = true;
+            }
+
+            if (this.keysPressed.includes(KEY_LEFT)) {
                 // Left
-                this.me.x--;
+                me.velocityX -= (SPEED_CHANGE * deltaTime) / (movingY ? diagonalModifier : 1);
             }
-            if (this.keysPressed.includes(39)) {
+            if (this.keysPressed.includes(KEY_RIGHT)) {
                 // Right
-                this.me.x++;
+                me.velocityX += (SPEED_CHANGE * deltaTime) / (movingY ? diagonalModifier : 1);
             }
-            if (this.keysPressed.includes(38)) {
+            if (this.keysPressed.includes(KEY_UP)) {
                 // Up
-                this.me.y--;
+                me.velocityY -= (SPEED_CHANGE * deltaTime) / (movingX ? diagonalModifier : 1);
             }
-            if (this.keysPressed.includes(40)) {
+            if (this.keysPressed.includes(KEY_DOWN)) {
                 // Down
-                this.me.y++;
+                me.velocityY += (SPEED_CHANGE * deltaTime) / (movingX ? diagonalModifier : 1);
             }
+
+            me.x += this.state.me.velocityX * deltaTime;
+            me.y += this.state.me.velocityY * deltaTime;
+
+            me.velocityX *= Math.pow(SLOWING, deltaTime);
+            me.velocityY *= Math.pow(SLOWING, deltaTime);
+
+            if (Math.abs(me.velocityX) < 0.1) {
+                me.velocityX = 0;
+            }
+            if (Math.abs(me.velocityY) < 0.1) {
+                me.velocityY = 0;
+            }
+
+            this.setState({ me });
         }
+    }
+
+    resize() {
+        const canvas = document.getElementById('game') as HTMLCanvasElement;
+
+        console.log('Resizing', canvas);
+
+        if (canvas) {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        }
+    }
+
+    onSceneMount(event: SceneEventArgs) {
+        const { scene } = event;
+
+        const camera = new UniversalCamera('Camera', new Vector3(0, 0, 20), scene);
+        camera.rotation = new Vector3(0, -Math.PI, Math.PI);
+
+        scene.getEngine().runRenderLoop(() => {
+            this.tick(scene.getEngine().getDeltaTime());
+
+            if (scene) {
+                scene.render();
+            }
+        });
     }
 
     render() {
         return (
             <>
-                <canvas
-                    id="game"
-                    width="640"
-                    height="480"
-                    style={{ border: '1px solid black' }}
-                    ref={this.gameCanvas}
-                />
+                <Engine antialias={true} canvasId="game">
+                    <Scene onSceneMount={(event) => this.onSceneMount(event)}>
+                        {Object.keys(this.state.players)
+                            .filter((key) => key !== this.index)
+                            .map((key, index) => (
+                                <plane
+                                    name="canvas"
+                                    size={1}
+                                    position={
+                                        new Vector3(
+                                            this.state.players[key].x * 0.01,
+                                            this.state.players[key].y * 0.01,
+                                            0,
+                                        )
+                                    }
+                                    sideOrientation={Mesh.BACKSIDE}
+                                    key={index}
+                                >
+                                    <advancedDynamicTexture
+                                        name="dialogTexture"
+                                        height={100}
+                                        width={100}
+                                        createForParentMesh={true}
+                                        hasAlpha={true}
+                                        generateMipMaps={true}
+                                        samplingMode={Texture.TRILINEAR_SAMPLINGMODE}
+                                    >
+                                        <rectangle background={'#AAFF00'} name="rect-1" height={1} width={1} />
+                                    </advancedDynamicTexture>
+                                </plane>
+                            ))}
+                        {this.state.me && (
+                            <plane
+                                name="canvas"
+                                size={1}
+                                position={new Vector3(this.state.me.x * 0.01, this.state.me.y * 0.01, 0)}
+                                sideOrientation={Mesh.BACKSIDE}
+                            >
+                                <advancedDynamicTexture
+                                    name="dialogTexture"
+                                    height={100}
+                                    width={100}
+                                    createForParentMesh={true}
+                                    hasAlpha={true}
+                                    generateMipMaps={true}
+                                    samplingMode={Texture.TRILINEAR_SAMPLINGMODE}
+                                >
+                                    <rectangle background={'#FF0000'} name="rect-1" height={1} width={1} />
+                                </advancedDynamicTexture>
+                            </plane>
+                        )}
+                    </Scene>
+                </Engine>
             </>
         );
     }

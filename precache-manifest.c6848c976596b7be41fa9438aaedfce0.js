89d8895f0299b754d63a814c9bbdfe14b42182c3
Jonáš Rosecký <rosecky.jonas@centrum.cz>
2020-09-14 23:21:35 +0300

    Updates

---
+++ precache-manifest.c6848c976596b7be41fa9438aaedfce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7967ccd6d92f4a1617acb26c8bcfcd",
+    "url": "/randomgame/index.html"
+  },
+  {
+    "revision": "b4ea2a53a28a2d1b784d",
+    "url": "/randomgame/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "71b97ceb6f0a1bfa1f4e",
+    "url": "/randomgame/static/js/2.64d2b451.chunk.js"
+  },
+  {
+    "revision": "3d6aadebc534851ffdef51a187c0403c",
+    "url": "/randomgame/static/js/2.64d2b451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ea2a53a28a2d1b784d",
+    "url": "/randomgame/static/js/main.68047046.chunk.js"
+  },
+  {
+    "revision": "b3aff5345bb83a26d4c8",
+    "url": "/randomgame/static/js/runtime-main.c5f14198.js"
+  }
+]);

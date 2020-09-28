d1d39ea6acdf2a8463b5502827fbcc25cb44ed10
Jonáš Rosecký <rosecky.jonas@centrum.cz>
2020-09-28 10:49:42 +0300

    Upkeeper changed to http from socket

--- src/backend/network/Upkeeper.ts
+++ src/backend/network/Upkeeper.ts
@@ -1,11 +1,15 @@
-import io from 'socket.io-client';
+import { Response } from "express";
 
 export class Upkeeper {
     constructor(upkeepURL: string, interval: number) {
-        const socket = io(upkeepURL);
+        const requestify = require('requestify');
+
+        console.log("Upkeeper set up!")
 
         setInterval(() => {
-            socket.emit('client_ping', 999);
+            const upkeeper = (requestify.get(upkeepURL) as Promise<Response>)
+            upkeeper.catch((reason) => console.error("upkeep ", reason.code));
+            upkeeper.then((response) => console.error("upkeep ", 200));
         }, interval);
     }
 }
